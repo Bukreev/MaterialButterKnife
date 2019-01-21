4a7f47e119441eed6938283a3eb03a2a3d6916e3
bukreevei <suzuka1100@mail.ru>
2019-01-21 22:26:37 +0300

    добавил вью и коллбеки

--- app/src/main/java/com/lesson2/bukreevei/materialbutterknife/view/userview/UserView.java
+++ app/src/main/java/com/lesson2/bukreevei/materialbutterknife/view/userview/UserView.java
@@ -3,48 +3,39 @@ package com.lesson2.bukreevei.materialbutterknife.view.userview;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.lesson2.bukreevei.materialbutterknife.R;
-import com.lesson2.bukreevei.materialbutterknife.view.userview.callback.UserViewInterface;
+import com.lesson2.bukreevei.materialbutterknife.view.userview.callback.UserViewCallBackInterface;
 
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import butterknife.BindView;
-import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.OnTextChanged;
 
 /**
  * TODO: document your custom view class.
  */
-public class UserView extends LinearLayout implements UserViewInterface {
+public class UserView extends LinearLayout implements UserViewCallBackInterface {
 
 
+    @BindView(R.id.material_text_button)
+    Button material_text_button;
     @BindView(R.id.user_name)
-    protected TextInputEditText mUserName;
+    TextInputEditText user_name;
     @BindView(R.id.user_email)
-    protected TextInputEditText mUserEmail;
+    TextInputEditText user_email;
     @BindView(R.id.user_phone)
-    protected TextInputEditText mUserPhone;
-
-    private final UserViewInterface binding;
+    TextInputEditText user_phone;
 
     public UserView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.sample_user_view, this, true);
+        material_text_button.setEnabled(false);
+        LayoutInflater.from(context).inflate(R.layout.user_view, this);
+
         setOrientation(VERTICAL);
-    }
-
-    public void bind() {
-        bind
-    }
-
-    @OnTextChanged({R.id.user_name, R.id.user_email, R.id.user_phone})
-    protected void onTextChange() {
-        if (textCallBack != null) {
-            textCallBack.textChangeCallBack();
-        }
     }
 
     @Override
@@ -52,8 +43,14 @@ public class UserView extends LinearLayout implements UserViewInterface {
 
     }
 
-    @Override
+    @OnTextChanged({R.id.user_name, R.id.user_email, R.id.user_phone})
     public void textChangeCallBack() {
+        if (user_name.getTextSize() > 0 && user_email.getTextSize() > 0 && user_phone.getTextSize() > 0)
+            material_text_button.setEnabled(true);
+    }
+
+    @OnClick(R.id.material_text_button)
+    public void setButtonCallBack() {
 
     }
 
--- app/src/main/java/com/lesson2/bukreevei/materialbutterknife/view/userview/callback/UserViewCallBackInterface.java
+++ app/src/main/java/com/lesson2/bukreevei/materialbutterknife/view/userview/callback/UserViewCallBackInterface.java
@@ -1,6 +1,7 @@
 package com.lesson2.bukreevei.materialbutterknife.view.userview.callback;
 
-public interface UserViewInterface  {
+public interface UserViewCallBackInterface  {
     void buttonCallback();
     void textChangeCallBack();
 }
+
